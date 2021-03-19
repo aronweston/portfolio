1829324fbe1e8e944811be9f717f8b7f2c0fa5ea
aronweston <aron@firststepdigital.com.au>
2021-03-20 02:05:25 +1100

    netlify forms with state

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'gatsby';
 import Button from './Button';
 
 const Footer = () => {
@@ -8,7 +7,32 @@ const Footer = () => {
   const [message, setMessage] = useState('');
   const [isSent, setIsSent] = useState(false);
 
-  const handleSubmit = (e) => {
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+      )
+      .join('&');
+  };
+
+  const handleSubmit = async (e) => {
+    const body = {
+      name,
+      company,
+      message,
+    };
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'portfolio form': 'portfolio form', body }),
+    })
+      .then(() => {
+        alert('Success!');
+        setIsSent(true);
+      })
+      .catch((error) => alert(error));
+
     e.preventDefault();
     console.log(name, company, message);
   };
@@ -16,15 +40,17 @@ const Footer = () => {
   return (
     <footer className='py-6 w-full bg-yellow-200 text-gray-900 text-center p-5'>
       <form
-        action='POST'
+        method='POST'
         data-netlify='true'
         name='portfolio form'
+        onSubmit={handleSubmit}
+        data-netlify-honeypot='bot-field'
         className='m-5 container w-1/2 mx-auto'>
         <input type='hidden' name='portfolio-form' value='portfolio form' />
-        <div className='grid grid-cols-1 gap-6'>
+        <div className='grid grid-cols-1 gap-3'>
           <label htmlFor='name' class='block'>
             <input
-              // onChange={(e) => setName(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
               type='text'
               name='name'
               class='mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0'
@@ -34,7 +60,7 @@ const Footer = () => {
           <label class='block' htmlFor='company'>
             <input
               type='text'
-              // onChange={(e) => setCompany(e.target.value)}
+              onChange={(e) => setCompany(e.target.value)}
               name='company'
               class='mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0'
               placeholder='Company or organisation'
@@ -43,13 +69,13 @@ const Footer = () => {
           <label className='block' htmlFor='message'>
             <textarea
               name='message'
-              // onChange={(e) => setMessage(e.target.value)}
+              onChange={(e) => setMessage(e.target.value)}
               className='mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0'
-              rows='3'></textarea>
+              rows='3'>
+              {message}
+            </textarea>
           </label>
-          <div class='block'>
-            <div data-netlify-recaptcha='true'></div>
-          </div>
+          <div data-netlify-recaptcha='true'></div>
           <Button>Send!</Button>
         </div>
       </form>
