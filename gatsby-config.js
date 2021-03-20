1f293d89914b8f5f75bb190d65b01f6fe0ec4edc
aronweston <aron@firststepdigital.com.au>
2021-03-20 18:45:51 +1100

    change manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,18 +11,18 @@ module.exports = {
         postCssPlugins: [require('tailwindcss')],
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'GatsbyJS',
-        short_name: 'GatsbyJS',
-        start_url: '/',
-        background_color: '#6b37bf',
-        theme_color: '#6b37bf',
-        display: 'standalone',
-        icon: 'src/assets/images/favicon/favicon.ico',
-        crossOrigin: `use-credentials`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: 'GatsbyJS',
+    //     short_name: 'GatsbyJS',
+    //     start_url: '/',
+    //     background_color: '#6b37bf',
+    //     theme_color: '#6b37bf',
+    //     display: 'standalone',
+    //     icon: 'src/assets/images/favicon/favicon.ico',
+    //     crossOrigin: `use-credentials`,
+    //   },
+    // },
   ],
 };
